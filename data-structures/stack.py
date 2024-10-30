65f5418409127aa0bfe337a8ecd6c20fb0f62c7d
Joe Williams <hey-there.joe@outlook.com>
2024-10-30 15:37:00 -0700

    updated property names

--- data-structures/stack.py
+++ data-structures/stack.py
@@ -7,58 +7,58 @@ from __future__ import annotations # for type hinting
 '''
 
 class Stack:
-  capacity: int
-  top: int # position of the top item in the stack
+  _capacity: int
+  _top: int # position of the top item in the stack
   # all items in the stack - we're using a list here, but in C++ this would be an array on the heap
   # that we create in the constructor
-  items: list[any]
+  _items: list[any]
 
   def __init__(self, capacity: int) -> None:
     if capacity < 1:
       raise ValueError('Stack size must be at least 1!')
     
-    self.capacity = capacity
+    self._capacity = capacity
     # "allocate" an array of the correct size - make sure you also delete this at the end!
-    self.items = [None] * self.capacity
+    self._items = [None] * self._capacity
 
     # set the top to the start of the array
-    self.top = 0
+    self._top = 0
 
   def push(self, item: any) -> None:
     ''' Adds an item to the top of the stack. '''
     # crash if the stack overflows
-    if self.top >= self.capacity:
+    if self._top >= self._capacity:
       raise RuntimeError('Stack overflow!')
     
     # insert the item and step to the next open space
-    self.items[self.top] = item
-    self.top += 1
+    self._items[self._top] = item
+    self._top += 1
 
   def pop(self) -> any:
     ''' Removes and returns the top item on the stack. '''
     # crash if the stack underflows
-    if self.top <= 0:
+    if self._top <= 0:
       raise RuntimeError('Stack underflow!')
     
     # we don't need to remove the item from the array (by setting it to None) because the only way
     # we can return that index in the array is if something was added there, which would overwrite
     # the item we just removed
-    self.top -= 1
-    return self.items[self.top]
+    self._top -= 1
+    return self._items[self._top]
 
   def peek(self) -> any:
     ''' Returns the top item on the stack without removing it, or None if the stack is empty. '''
-    if self.top <= 0:
+    if self._top <= 0:
       return None
-    return self.items[self.top - 1]
+    return self._items[self._top - 1]
 
   def isEmpty(self) -> bool:
     ''' Self-explanatory. '''
-    return self.top <= 0
+    return self._top <= 0
 
   def isFull(self) -> bool:
     ''' Self-explanatory. '''
-    return self.top <= self.capacity
+    return self._top <= self._capacity
   
 stack = Stack(10)
 print('created stack')
